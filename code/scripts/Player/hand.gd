9fde7aa51adb6abadfae75dad774ecbf0fa540b4
Nanito111 <fgaldames79@gmail.com>
2024-05-18 13:22:01 -0400

    ADD: show focus hand item

--- code/scripts/Player/hand.gd
+++ code/scripts/Player/hand.gd
@@ -3,6 +3,8 @@ extends RayCast3D
 
 var inventory:Inventory = Inventory.new()
 var current_hand_item:int = 0
+var hands_items:Array[Node3D] = []
+@export var hand_item_pivot:Node3D
 
 enum PickerStates {
 	ON,
@@ -49,8 +51,40 @@ func hand_inventory_input(event:InputEvent):
 		current_hand_item = 3
 	elif ((event.is_action("p_hand_slot_4"))):
 		current_hand_item = 4
+	
+	
+	if hands_items[last_hand_item] != null:
+		hands_items[last_hand_item].hide()
+
+	if hands_items[current_hand_item] != null:
+		hands_items[current_hand_item].show()
 
 	hand_slot_changed.emit(current_hand_item, last_hand_item)
+
+func add_hand_item(item_index:int, _inventory):
+	if item_index > 4:
+		return
+
+	hands_items[item_index] = inventory.content[item_index].item_resource.hand_instance.instantiate()
+	hand_item_pivot.add_child(hands_items[item_index])
+
+	if item_index == current_hand_item:
+		hands_items[item_index].show()
+	else:
+		hands_items[item_index].hide()
+
+func remove_hand_item(item_index:int):
+	if item_index > 4:
+		return
+
+	hands_items[item_index].queue_free()
+	hands_items[item_index] = null
+
+func _ready():
+	hands_items.resize(5)
+	hands_items.fill(null)
+	inventory.item_added.connect(add_hand_item)
+	inventory.item_removed.connect(remove_hand_item)
 
 func _physics_process(_delta):
 	picker_handler()
